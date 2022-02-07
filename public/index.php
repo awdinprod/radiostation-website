ef0fabb047c12dbc46628013bdddc88b78aad84f
Artemiy Doronin <artemiy.ev.doronin@gmail.com>
2022-02-07 12:13:35 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+require ('/home/awdinprod/PhpstormProjects/radiodawn.fm/header.php');
+
+require ('/home/awdinprod/PhpstormProjects/radiodawn.fm/mainpage.php');
+
+require ('/home/awdinprod/PhpstormProjects/radiodawn.fm/footer.php');
