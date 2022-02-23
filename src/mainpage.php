6ae493b24c34058bf13138612f360b8632b54db2
Artemiy Doronin <artemiy.ev.doronin@gmail.com>
2022-02-23 17:03:32 +0700

    design updated

--- src/mainpage.php
+++ src/mainpage.php
@@ -1,10 +1,10 @@
-<div class="banner" style="font-size: 30pt;">
+<div class="banner" style="font-size: 30pt; border: 2px solid #222238;">
     If you see this text then this banner is now unavailable and sad. <br> But you'll be able to all cool images here pretty soon. Betcha :)
 </div>
 
-<div class="banner" style="font-size: 30pt; height: 600px; margin-top: 20px;"> I know you would like something to read. <br> We're currently working on it</div>
+<div class="banner" style="font-size: 30pt; height: 600px; margin-top: 20px; border: 2px solid #222238;"> I know you would like something to read. <br> We're currently working on it</div>
 
 <div class="column">
-    <div class="podcasts" style="font-size: 30pt;"> Podcasts will be available soon. <br> Prepare your headphones</div>
-    <div class="chart" style="font-size: 30pt;"> Look at this amazing chart... oh, coming soon. <br> (damn, I forgot) </div>
+    <div class="podcasts" style="font-size: 30pt; border: 2px solid #222238;"> Podcasts will be available soon. <br> Prepare your headphones</div>
+    <div class="chart" style="font-size: 30pt; border: 2px solid #222238;"> Look at this amazing chart... oh, coming soon. <br> (damn, I forgot) </div>
 </div>
