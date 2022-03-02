2a49add9fce4d58d1da790537f2ab3f84b24d94d
Artemiy Doronin <artemiy.ev.doronin@gmail.com>
2022-03-02 15:19:28 +0700

    Update main page

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php
+//    echo $_SERVER['REQUEST_URI'];
     $uri = $_SERVER['REQUEST_URI'];
-    if ($uri == '/index.php' || $uri == '/') {
-        require ('src/header.php');
-        require ('src/mainpage.php');
-        require ('src/footer.php');
+    if ($uri == '/' || $uri == '/index.php') {
+        require('src/header.php');
+        require('src/mainpage.php');
+        require('src/footer.php');
     }
-    elseif ($uri == '/online-player/player.html') {
-        require ('online-player/player.html');
+    elseif ($uri == '/online-player/') {
+        require('online-player/player.html');
+    }
+    elseif ($uri == '/posts') {
+        require('posts.php');
+    }
+    elseif ($uri == '/podcasts') {
+        require('podcasts.php');
+    }
+    elseif ($uri == '/chart') {
+        require('chart.php');
     }
     else {
         echo "404: Not Found :(";
     }
-?>
--- src/header.php
+++ src/header.php
@@ -4,14 +4,14 @@
     <head>
         <meta charset="UTF-8">
         <title>DAWN FM</title>
-        <link href="/src/style.css" rel="stylesheet">
+        <link href="/style.css" type="text/css" rel="stylesheet">
 
     </head>
 
     <body>
         <header>
             <ul class="header">
-                <li class="hor-header"><a href="index.php">main page</a></li>
+                <li class="hor-header"><a href="/index.php">main page</a></li>
                 <li class="hor-header"><a href="/online-player/player.html" target="_blank" rel="noopener noreferrer">online player</a></li>
                 <li class="hor-header"><a href="/posts.php">posts</a></li>
                 <li class="hor-header"><a href="/podcasts.php">podcasts</a></li>
