4f25e4b60194ad81fe22201e2ef18441b160105e
Artemiy Doronin <artemiy.ev.doronin@gmail.com>
2022-04-18 15:19:11 +0700

    US-7 - Make a customized podcast player on episode page

--- App/Templates/singlepodcastpage.php
+++ App/Templates/singlepodcastpage.php
@@ -6,10 +6,38 @@
         <div class="post-texts">
             <p class="post-title"><?php echo $singlecontentarray['title'];?></p>
             <p class="post-excerpt"><?php echo $singlecontentarray['shorttext'];?></p>
-            <audio controls>
-                <source src="<?php echo $singlecontentarray['audiourl'];?>">
-            </audio>
+            <audio data-src="<?php echo $singlecontentarray['audiourl'];?>" type="audio/mpeg"></audio>
+            <div class="audio-player">
+                <div class="timeline">
+                    <div class="progress"></div>
+                </div>
+                <div class="controls">
+                    <div class="play-container">
+                        <div class="toggle-play play">
+                        </div>
+                    </div>
+                    <div class="time">
+                        <div class="current">0:00</div>
+                        <div class="divider">/</div>
+                        <div class="length"></div>
+                    </div>
+                    <div class="name"><?php echo $singlecontentarray['title'];?></div>
+
+                    <div class="volume-container">
+                        <div class="volume-button">
+                            <div class="volume icono-volumeMedium"></div>
+                        </div>
+
+                        <div class="volume-slider">
+                            <div class="volume-percentage"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <hr class="post-divider">
         </div>
     </div>
 </div>
+
+<script type="text/javascript" src="/JS/audioplayer.js"></script>
