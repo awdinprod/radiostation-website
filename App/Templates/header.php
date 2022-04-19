086e886691964ea48596968432ec9099b21433e5
Artemiy Doronin <artemiy.ev.doronin@gmail.com>
2022-04-19 18:53:40 +0700

    Refactoring

--- App/Templates/header.php
+++ App/Templates/header.php
@@ -4,6 +4,5 @@
         <li class="hor-header"><a href="/online-player" target="_blank" rel="noopener noreferrer">online player</a></li>
         <li class="hor-header"><a href="/posts">posts</a></li>
         <li class="hor-header"><a href="/podcasts">podcasts</a></li>
-<!--        <li class="hor-header"><a href="/chart">music chart</a></li>-->
     </ul>
 </header>
